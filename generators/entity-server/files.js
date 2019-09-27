0cc950a7094d4d611a147b82b23d1f50bec47bb4
joewhite101 <j.white@entando.com>
2019-09-27 17:50:37 +0200

    Update folder generation for individual widgets

--- generators/entity-server/files.js
+++ generators/entity-server/files.js
@@ -4,7 +4,6 @@ const _ = require('lodash');
 const constants = require('generator-jhipster/generators/generator-constants');
 
 const SERVER_MAIN_SRC_DIR = constants.SERVER_MAIN_SRC_DIR;
-const UI_SRC_DIR = constants.SERVER_MAIN_SRC_DIR;
 
 const serverFiles = {
     server: [
@@ -26,7 +25,6 @@ const serverFiles = {
     ]
 };
 
-
 const microFrontEndFiles = {
     server: [
         {
@@ -34,36 +32,38 @@ const microFrontEndFiles = {
             templates: [
                 {
                     file: '/tableWidget/src/types/entityType.js',
-                    renameTo: generator => `/tableWidget/src/types/${generator.entityClass.charAt(0).toLowerCase() + generator.entityClass.slice(1)}Type.js`,
+                    renameTo: generator => `/${generator.entityInstance}/tableWidget/src/types/${generator.entityInstance}Type.js`,
                     useBluePrint: true
                 },
                 {
                     file: '/tableWidget/package.json',
+                    renameTo: generator => `/${generator.entityInstance}/tableWidget/package.json`,
                     method: 'copy'
                 },
                 {
                     file: '/common/jsconfig.json',
-                    renameTo: generator => `/tableWidget/jsconfig.json`,
+                    renameTo: generator => `/${generator.entityInstance}/tableWidget/jsconfig.json`,
                     method: 'copy'
                 },
                 {
                     file: '/tableWidget/src/index.js',
-                    renameTo: generator => `/tableWidget/src/index.js`,
+                    renameTo: generator => `/${generator.entityInstance}/tableWidget/src/index.js`,
                     useBluePrint: true
                 },
                 {
                     file: '/tableWidget/src/index.css',
+                    renameTo: generator => `/${generator.entityInstance}/tableWidget/src/index.css`,
                     method: 'copy'
                 },
                 //Mocks
                 {
                     file: '/tableWidget/src/mocks/entityMocks.js',
-                    renameTo: generator => `/tableWidget/src/mocks/${generator.entityClass.charAt(0).toLowerCase() + generator.entityClass.slice(1)}Mocks.js`,
+                    renameTo: generator => `/${generator.entityInstance}/tableWidget/src/mocks/${generator.entityInstance}Mocks.js`,
                     useBluePrint: true
                 },
                 {
                     file: '/tableWidget/src/mocks/entityi18nMock.js',
-                    renameTo: generator => `/tableWidget/src/mocks/${generator.entityClass.charAt(0).toLowerCase() + generator.entityClass.slice(1)}i18nMock.js`,
+                    renameTo: generator => `/${generator.entityInstance}/tableWidget/src/mocks/${generator.entityInstance}i18nMock.js`,
                     useBluePrint: true
                 },
 
@@ -71,51 +71,51 @@ const microFrontEndFiles = {
 
                 {
                     file: '/tableWidget/src/i18n/locales.js',
-                    renameTo: generator => `/tableWidget/src/i18n/locales.js`,
+                    renameTo: generator => `/${generator.entityInstance}/tableWidget/src/i18n/locales.js`,
                     method: 'copy'
                 },
                 {
                     file: '/tableWidget/src/i18n/index.js',
-                    renameTo: generator => `/tableWidget/src/i18n/index.js`,
+                    renameTo: generator => `/${generator.entityInstance}/tableWidget/src/i18n/index.js`,
                     method: 'copy'
                 },
                 {
                     file: '/tableWidget/src/i18n/i18nextOptions.js',
-                    renameTo: generator => `/tableWidget/src/i18n/i18nextOptions.js`,
+                    renameTo: generator => `/${generator.entityInstance}/tableWidget/src/i18n/i18nextOptions.js`,
                     method: 'copy'
                 },
                 {
                     file: '/tableWidget/src/i18n/locales/it.json',
-                    renameTo: generator => `/tableWidget/src/i18n/locales/it.json`,
+                    renameTo: generator => `/${generator.entityInstance}/tableWidget/src/i18n/locales/it.json`,
                     useBluePrint: true
                 },
                 {
                     file: '/tableWidget/src/i18n/locales/en.json',
-                    renameTo: generator => `/tableWidget/src/i18n/locales/en.json`,
+                    renameTo: generator => `/${generator.entityInstance}/tableWidget/src/i18n/locales/en.json`,
                     useBluePrint: true
                 },
 
                 //custom elements
                 {
                     file: '/tableWidget/src/custom-elements/entityTableElement.js',
-                    renameTo: generator => `/tableWidget/src/custom-elements/${generator.entityClass}TableElement.js`,
+                    renameTo: generator => `/${generator.entityInstance}/tableWidget/src/custom-elements/${generator.entityClass}TableElement.js`,
                     useBluePrint: true
                 },
 
                 //components
                 {
                     file: '/tableWidget/src/components/entityTableContainer.js',
-                    renameTo: generator => `/tableWidget/src/components/${generator.entityClass}TableContainer.js`,
+                    renameTo: generator => `/${generator.entityInstance}/tableWidget/src/components/${generator.entityClass}TableContainer.js`,
                     useBluePrint: true
                 },
                 {
                     file: '/tableWidget/src/components/entityTable.js',
-                    renameTo: generator => `/tableWidget/src/components/${generator.entityClass}Table.js`,
+                    renameTo: generator => `/${generator.entityInstance}/tableWidget/src/components/${generator.entityClass}Table.js`,
                     useBluePrint: true
                 },
                 {
                     file: '/tableWidget/src/components/common/ErrorNotification.js',
-                    renameTo: generator => `/tableWidget/src/components/common/ErrorNotification.js`,
+                    renameTo: generator => `/${generator.entityInstance}/tableWidget/src/components/common/ErrorNotification.js`,
                     useBluePrint: true
                 },
 
@@ -135,25 +135,29 @@ const microFrontEndFiles = {
                 //api
                 {
                     file: '/tableWidget/src/api/entityApi.js',
-                    renameTo: generator => `/tableWidget/src/api/${generator.entityClass.charAt(0).toLowerCase() + generator.entityClass.slice(1)}s.js`,
+                    renameTo: generator => `/${generator.entityInstance}/tableWidget/src/api/${generator.entityInstancePlural}.js`,
                     useBluePrint: true
                 },
                 {
                     file: '/tableWidget/src/api/constants.js',
+                    renameTo: generator => `/${generator.entityInstance}/tableWidget/src/api/constants.js`,
                     method: 'copy'
                 },
 
                 //public
                 {
                     file: '/tableWidget/public/favicon.ico',
+                    renameTo: generator => `/${generator.entityInstance}/tableWidget/public/favicon.ico`,
                     method: 'copy'
                 },
                 {
                     file: '/tableWidget/public/index.html',
+                    renameTo: generator => `/${generator.entityInstance}/tableWidget/public/index.html`,
                     useBluePrint: true
                 },
                 {
                     file: '/tableWidget/public/robots.txt',
+                    renameTo: generator => `/${generator.entityInstance}/tableWidget/public/robots.txt`,
                     method: 'copy'
                 },
 
