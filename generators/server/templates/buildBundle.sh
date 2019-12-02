fb36dcd18c97cf4faad505115567f98eff02e529
Kerruba <luca.cherubin@gmail.com>
2019-12-02 13:12:45 +0100

    Makes the build script for robust

--- generators/server/templates/buildBundle.sh
+++ generators/server/templates/buildBundle.sh
@@ -1,34 +1,41 @@
 #!/bin/bash
 
-INJECTION_POINT="<#-- entando_resource_injection_point -->"
+function createFolderTree() {
+    local widgetFolder="$1"
+
+    echo "Creating folder structure for $widgetFolder"
+    mkdir -p bundle/"$widgetFolder"/resources/static/{js,css}
+
+    # Copy bundle metadata and template
+    cp "$widgetFolder"/bundle/* bundle/"$widgetFolder"/
+
+    # Copying resources for widgets
+    cp "$widgetFolder"/build/static/js/*.js bundle/"$widgetFolder"/resources/static/js
+    cp "$widgetFolder"/build/static/css/*.css bundle/"$widgetFolder"/resources/static/css 2>/dev/null
+
+    local cssExitStatus=$?
+    if [ $cssExitStatus -ne 0 ]; then
+        echo "WARN: no css found for $widgetFolder"
+    fi
+
+}
 
 function injectResource() {
     local resource="$1"
     local destFile="$2"
 
     echo "Injecting resource $resource in $destFile"
-    sed -i 's,'"$INJECTION_POINT"','"$resource"'\n'"$INJECTION_POINT"',g' "$destFile"
+    sed -i 's|'"$INJECTION_POINT"'|'"$resource"'\n'"$INJECTION_POINT"'|g' "$destFile"
 }
 
-# This command assumes that the widgets are all under ui/widgets/<entity>/<widget>. The command finds all of the micro-frontends in those folders and
-# copies the result of the build into the bundle resources folder so that the bundle can be deployed to a digital exchange instance (or imported on an existing page).
-# The command also copies css optionally with this structure since some widgets will be js only 2>/dev/null || :
+function updateFTLTemplate() {
+    shopt -s nullglob # Set the results of globs in forloop to emptylist if no file is found
+    local dir="$1"
+    local artifactId="$2"
 
-find ui/widgets -maxdepth 2 -mindepth 2 -type d -not -path "*utils*" -exec bash -c "mkdir -p bundle/'{}'/resources/static/{js,css} && cp '{}'/build/static/js/*.js bundle/'{}'/resources/static/js && cp '{}'/build/static/css/*.css bundle/'{}'/resources/static/css 2>/dev/null || : && mkdir -p bundle/{} && cp '{}'/bundle/* bundle/{}/" \;
-
-#Fetch the top level service name from the pom and use this as the context directory for the publishing of assets specific to the project when building the bundle
-artifactId=$(cat pom.xml | grep "^    <artifactId>.*</artifactId>$" | awk -F'[><]' '{print $3}')
-
-echo "Generating bundle for service ${artifactId}"
-
-mkdir -p bundle/resources/static/{js,css}
-
-# For each widget under the structure ui/widgets/<entity>/<widget> generate an FTL file that imports the css and js that goes with that widget.
-# The FTL file from the widget itself is preserved and the imports are added at the top of the widget
-for dir in `find bundle/ui/widgets -maxdepth 2 -mindepth 2 -type d -not -path "*utils*" -exec bash -c "cd {} && pwd" \;`;
-do
-    widgetName=$(basename $dir)
+    widgetName=$(basename "$dir")
     echo "Generating bundle ${widgetName} for $dir"
+
     for ftlName in "$dir"/*.ftl;
     do
         [ -e "$ftlName" ] || continue
@@ -36,8 +43,8 @@ do
         for jspath in "$dir"/resources/static/js/*;
         do
             # This moves the referenced file to the top level bundle/resources/static dir for correct processing when loaded
-            [ -e "$jspath" ] || continue
             jsfile=$(basename "$jspath")
+
 
             cp "$dir/resources/static/js/$jsfile" bundle/resources/static/js/
             resource="<script src=\"<@wp.resourceURL />${artifactId}/static/js/${jsfile}\"></script>"
@@ -47,9 +54,10 @@ do
         # For every CSS file add a script reference in the widget FTL
         for csspath in "$dir"/resources/static/css/*;
         do
+
           # This moves the referenced file to the top level bundle/resources/static dir for correct processing when loaded
-          [ -e "$csspath" ] || continue
           cssfile=$(basename "$csspath")
+
           cp "$dir/resources/static/css/$cssfile" bundle/resources/static/css/
           resource="<link href=\"<@wp.resourceURL />${artifactId}/static/css/${cssfile}\" rel=\"stylesheet\">"
           injectResource "$resource" "$ftlName"
@@ -58,4 +66,28 @@ do
 
     #Cleanup the resources that were copied into the widget folders specifically. They are now copied into the main bundle folder
     rm -rf "$dir/resources"
-done
+    shopt -u nullglob
+
+}
+
+export -f createFolderTree
+export -f injectResource
+export -f updateFTLTemplate
+export INJECTION_POINT="<#-- entando_resource_injection_point -->"
+
+# This command assumes that the widgets are all under ui/widgets/<entity>/<widget>. The command finds all of the micro-frontends in those folders and
+# copies the result of the build into the bundle resources folder so that the bundle can be deployed to a digital exchange instance (or imported on an existing page).
+# The command also copies css optionally with this structure since some widgets will be js only 2>/dev/null || :
+
+find ui/widgets -maxdepth 2 -mindepth 2 -type d -not -path "*utils*" -exec bash -c 'createFolderTree "$@"' bash {} \;
+
+mkdir -p bundle/resources/static/{js,css}
+
+#Fetch the top level service name from the pom and use this as the context directory for the publishing of assets specific to the project when building the bundle
+artifactId=$(awk -F'[><]' '/<artifactId>.*<\/artifactId>/ {print $3; exit}' pom.xml)
+
+echo "Updating bundle for service ${artifactId}"
+# For each widget under the structure ui/widgets/<entity>/<widget> generate an FTL file that imports the css and js that goes with that widget.
+# The FTL file from the widget itself is preserved and the imports are added at the top of the widget
+find bundle/ui/widgets -maxdepth 2 -mindepth 2 -type d -not -path "*utils*" -exec bash -c 'updateFTLTemplate "$@"' bash {} "$artifactId" \;
+
