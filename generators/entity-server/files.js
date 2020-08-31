f5a2529169ec59798e69df6a0de4f746e484880c
Kerruba <luca.cherubin@gmail.com>
2020-08-31 17:58:03 +0200

    ENG-207 adds a first implementation of the entity class if no database is provided

--- generators/entity-server/files.js
+++ generators/entity-server/files.js
@@ -3,7 +3,7 @@ const constants = require('generator-jhipster/generators/generator-constants');
 const { SERVER_MAIN_SRC_DIR } = constants;
 
 const serverFiles = {
-  server: [
+  schema: [
     {
       path: SERVER_MAIN_SRC_DIR,
       templates: [
@@ -11,13 +11,25 @@ const serverFiles = {
           file: 'package/web/rest/schema/EntitySchemaResource.java',
           renameTo: generator =>
             `${generator.packageFolder}/web/rest/schema/${generator.entityClass}SchemaResource.java`,
-          useBluePrint: true,
+        },
+      ],
+    },
+  ],
+  entity: [
+    {
+      condition: generator => generator.databaseType === 'no',
+      path: SERVER_MAIN_SRC_DIR,
+      templates: [
+        {
+          file: 'package/domain/NoDbEntity.java',
+          renameTo: generator => `${generator.packageFolder}/domain/${generator.entityClass}.java`,
         },
       ],
     },
   ],
   repository: [
     {
+      condition: generator => generator.databaseType === 'no',
       path: SERVER_MAIN_SRC_DIR,
       templates: [
         {
@@ -37,10 +49,7 @@ const serverFiles = {
   ],
   service: [
     {
-      condition: generator => {
-        console.log(generator.service);
-        return generator.service === 'serviceImpl';
-      },
+      condition: generator => generator.databaseType === 'no' && generator.service === 'serviceImpl',
       path: SERVER_MAIN_SRC_DIR,
       templates: [
         {
@@ -57,38 +66,37 @@ const serverFiles = {
       ],
     },
     {
-      condition: generator => generator.service === 'serviceClass' && !generator.embedded,
+      condition: generator => generator.databaseType === 'no' && generator.service === 'serviceClass',
       path: SERVER_MAIN_SRC_DIR,
       templates: [
         {
           file: 'package/service/impl/NoDbServiceImpl.java',
-          renameTo: generator =>
-            `${generator.packageFolder}/service/${generator.entityClass}Service.java`,
-            // `${generator.packageFolder}/service/impl/${generator.entityClass}ServiceImpl.java`,
+          renameTo: generator => `${generator.packageFolder}/service/${generator.entityClass}Service.java`,
           override: true,
         },
       ],
     },
   ],
-  controller: [
-    {
-      path: SERVER_MAIN_SRC_DIR,
-      templates: [
-        {
-          file: 'package/repository/NoDbEntityRepository.java',
-          renameTo: generator =>
-            `${generator.packageFolder}/repository/${generator.entityClass}Repository.java`,
-          override: true,
-        },
-        {
-          file: 'package/repository/impl/NoDbEntityRepositoryImpl.java',
-          renameTo: generator =>
-            `${generator.packageFolder}/repository/impl/${generator.entityClass}RepositoryImpl.java`,
-          override: true,
-        },
-      ],
-    },
-  ],
+  // controller: [
+  //   {
+  //     condition: generator => generator.databaseType === 'no',
+  //     path: SERVER_MAIN_SRC_DIR,
+  //     templates: [
+  //       {
+  //         file: 'package/repository/NoDbEntityRepository.java',
+  //         renameTo: generator =>
+  //           `${generator.packageFolder}/repository/${generator.entityClass}Repository.java`,
+  //         override: true,
+  //       },
+  //       {
+  //         file: 'package/repository/impl/NoDbEntityRepositoryImpl.java',
+  //         renameTo: generator =>
+  //           `${generator.packageFolder}/repository/impl/${generator.entityClass}RepositoryImpl.java`,
+  //         override: true,
+  //       },
+  //     ],
+  //   },
+  // ],
 };
 
 module.exports = {
