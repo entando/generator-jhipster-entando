5e09d5dd935fd368251899af9f85354b5952fc71
Anthony Viard <anth.viard@gmail.com>
2020-09-14 11:26:20 +0200

    Remove unused method.

--- generators/server/phases/writing/index.js
+++ generators/server/phases/writing/index.js
@@ -31,12 +31,6 @@ function addMavenSnapshotRepository() {
   this.addMavenRepository('snapshot-repo', 'https://oss.sonatype.org/content/repositories/snapshots');
 }
 
-// TODO Remove this method if we don't need it anymore
-// eslint-disable-next-line
-function addEntandoAuthDependencies() {
-  this.addMavenDependency('io.github.openfeign', 'feign-jackson', null, null);
-}
-
 module.exports = {
   ...writeFiles(),
 
