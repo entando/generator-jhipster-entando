33d8c14ee1da163647cc980507abcd5ad2661d72
Anthony Viard <anth.viard@gmail.com>
2020-10-15 13:42:20 +0200

    Update expected-files.js to match with the current blueprint state

--- test/utils/expected-files.js
+++ test/utils/expected-files.js
@@ -1,17 +1,10 @@
-const constants = require('generator-jhipster/generators/generator-constants');
-
-const { SERVER_MAIN_SRC_RES, SERVER_MAIN_SRC_DIR } = constants;
-
 const expectedFiles = {
   server: [
-    `${SERVER_MAIN_SRC_RES}config/application.yml`,
-    `${SERVER_MAIN_SRC_DIR}com/mycompany/myapp/client/EntandoAuthClient.java`,
-    `${SERVER_MAIN_SRC_DIR}com/mycompany/myapp/config/EntandoPluginConfig.java`,
-    `${SERVER_MAIN_SRC_DIR}com/mycompany/myapp/config/EntandoPluginConfigManager.java`,
-    `${SERVER_MAIN_SRC_DIR}com/mycompany/myapp/config/ConfigServiceConfiguration.java`,
-    `${SERVER_MAIN_SRC_DIR}com/mycompany/myapp/config/EntandoProperties.java`,
-    `${SERVER_MAIN_SRC_DIR}com/mycompany/myapp/EntandoPluginApp.java`,
-    `${SERVER_MAIN_SRC_DIR}com/mycompany/myapp/web/rest/schema/EntandoPluginConfigSchemaResource.java`,
+    'bundle/descriptor.yaml',
+    'prepareMicrofrontends.sh',
+    'prepareBundle.sh',
+    'prepareDockerImage.sh',
+    'buildBundle.sh',
   ],
 };
 
