b27af731a9d5c8a93bce363c82901d97096e14d1
Anthony Viard <anth.viard@gmail.com>
2020-10-15 13:43:38 +0200

    Add test on the pom.xml and check Entando dependencies are present

--- test/server.spec.js
+++ test/server.spec.js
@@ -1,18 +1,12 @@
-const _ = require('lodash');
-const YAML = require('yaml');
-const fs = require('fs');
 const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 const expectedFiles = require('./utils/expected-files');
 
-function readFile(filename, json) {
-  const file = fs.readFileSync(filename, 'utf8');
-  return json ? JSON.parse(file) : file;
-}
+const appBaseName = 'entandoPlugin';
 
 describe('Subgenerator server of entando JHipster blueprint', () => {
-  describe('Sample test', () => {
+  describe('Default blueprint configuration', () => {
     before(done => {
       helpers
         .run('generator-jhipster/generators/server')
@@ -30,7 +24,7 @@ describe('Subgenerator server of entando JHipster blueprint', () => {
           ],
         ])
         .withPrompts({
-          baseName: 'entandoPlugin',
+          baseName: appBaseName,
           packageName: 'com.mycompany.myapp',
           applicationType: 'microservice',
           databaseType: 'sql',
@@ -49,6 +43,68 @@ describe('Subgenerator server of entando JHipster blueprint', () => {
 
     it('creates expected files for the blueprint', () => {
       assert.file(expectedFiles.server);
+      assert.file(`bundle/plugins/${appBaseName.toLowerCase()}-plugin.yaml`);
+    });
+
+    it('pom.xml contains the javax servlet dependency', () => {
+      assert.fileContent(
+        'pom.xml',
+        '        <dependency>\n' +
+          '            <groupId>javax.servlet</groupId>\n' +
+          '            <artifactId>javax.servlet-api</artifactId>\n' +
+          '        </dependency>\n',
+      );
+    });
+
+    it('pom.xml contains the Undertow dependency', () => {
+      assert.fileContent(
+        'pom.xml',
+        '        <dependency>\n' +
+          '            <groupId>org.springframework.boot</groupId>\n' +
+          '            <artifactId>spring-boot-starter-undertow</artifactId>\n' +
+          '<scope>provided</scope>\n' +
+          '        </dependency>\n',
+      );
+    });
+
+    it('pom.xml contains the Scala-library dependency', () => {
+      assert.fileContent(
+        'pom.xml',
+        '        <dependency>\n' +
+          '            <groupId>org.scala-lang</groupId>\n' +
+          '            <artifactId>scala-library</artifactId>\n' +
+          '            <version>2.12.1</version>\n' +
+          '        </dependency>\n',
+      );
+    });
+
+    it('pom.xml contains the mbknor-jackson-jsonschema dependecy', () => {
+      assert.fileContent(
+        'pom.xml',
+        '        <dependency>\n' +
+          '            <groupId>com.kjetland</groupId>\n' +
+          '            <artifactId>mbknor-jackson-jsonschema_2.12</artifactId>\n' +
+          '            <version>1.0.34</version>\n' +
+          '\n' +
+          '          <exclusions>\n' +
+          '              <exclusion>\n' +
+          '                  <groupId>org.scala-lang</groupId>\n' +
+          '                  <artifactId>scala-library</artifactId>\n' +
+          '              </exclusion>\n' +
+          '          </exclusions>\n' +
+          '          \n' +
+          '        </dependency>\n',
+      );
+    });
+
+    it('pom.xml contains the snapshot repository', () => {
+      assert.fileContent(
+        'pom.xml',
+        '        <repository>\n' +
+          '            <id>snapshot-repo</id>\n' +
+          '            <url>https://oss.sonatype.org/content/repositories/snapshots</url>\n' +
+          '        </repository>\n',
+      );
     });
   });
 });
