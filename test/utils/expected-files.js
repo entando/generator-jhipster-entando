2e29f8f2012847cfcff34a04ee16c0c79f2cbfc7
Anthony Viard <anth.viard@gmail.com>
2020-10-15 17:13:32 +0200

    Add SpringFox config file in the expected-files.js folder

--- test/utils/expected-files.js
+++ test/utils/expected-files.js
@@ -1,6 +1,6 @@
 const constants = require('generator-jhipster/generators/generator-constants');
 
-const { SERVER_MAIN_RES_DIR } = constants;
+const { SERVER_MAIN_RES_DIR, SERVER_MAIN_SRC_DIR } = constants;
 
 const expectedFiles = {
   microservices: [`${SERVER_MAIN_RES_DIR}static/favicon.png`],
@@ -10,6 +10,7 @@ const expectedFiles = {
     'prepareBundle.sh',
     'prepareDockerImage.sh',
     'buildBundle.sh',
+    `${SERVER_MAIN_SRC_DIR}com/mycompany/myapp/config/SpringFoxConfiguration.java`,
   ],
 };
 
