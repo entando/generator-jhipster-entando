1b3ea322abb7ec0d661622173a5e9d7a72901c86
Anthony Viard <anth.viard@gmail.com>
2020-10-16 13:58:48 +0200

    Add test on entity subgen expected files

--- test/utils/expected-files.js
+++ test/utils/expected-files.js
@@ -1,6 +1,6 @@
 const constants = require('generator-jhipster/generators/generator-constants');
 
-const { SERVER_MAIN_RES_DIR, SERVER_MAIN_SRC_DIR } = constants;
+const { SERVER_MAIN_RES_DIR, SERVER_MAIN_SRC_DIR, SERVER_TEST_SRC_DIR } = constants;
 
 const expectedFiles = {
   microservices: [`${SERVER_MAIN_RES_DIR}static/favicon.png`],
@@ -12,6 +12,21 @@ const expectedFiles = {
     'buildBundle.sh',
     `${SERVER_MAIN_SRC_DIR}com/mycompany/myapp/config/SpringFoxConfiguration.java`,
   ],
+  entity: {
+    server: {
+      common: [`${SERVER_MAIN_SRC_DIR}com/mycompany/myapp/web/rest/schema/FooSchemaResource.java`],
+      noDb: [
+        `${SERVER_MAIN_SRC_DIR}com/mycompany/myapp/domain/Foo.java`,
+        `${SERVER_MAIN_SRC_DIR}com/mycompany/myapp/repository/FooRepository.java`,
+        `${SERVER_MAIN_SRC_DIR}com/mycompany/myapp/repository/impl/FooRepositoryImpl.java`,
+        `${SERVER_MAIN_SRC_DIR}com/mycompany/myapp/web/rest/FooResource.java`,
+        `${SERVER_TEST_SRC_DIR}com/mycompany/myapp/domain/FooTest.java`,
+        `${SERVER_TEST_SRC_DIR}com/mycompany/myapp/web/rest/FooResourceIT.java`,
+      ],
+      serviceClass: [`${SERVER_MAIN_SRC_DIR}com/mycompany/myapp/service/FooService.java`],
+      serviceImpl: [`${SERVER_MAIN_SRC_DIR}com/mycompany/myapp/service/impl/FooServiceImpl.java`],
+    },
+  },
 };
 
 module.exports = expectedFiles;
