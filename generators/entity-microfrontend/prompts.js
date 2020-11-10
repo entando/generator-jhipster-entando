7de49ce72e378ba29dadb3d4ff80928d55477135
Anthony Viard <anth.viard@gmail.com>
2020-11-10 16:00:20 +0100

    Use async/await function in microfrontend prompts

--- generators/entity-microfrontend/prompts.js
+++ generators/entity-microfrontend/prompts.js
@@ -1,25 +1,19 @@
-function askForMfeGeneration(meta) {
-  // defaulting to asking about MFE generation
-  const { generateMicroFrontends } = this;
-
+async function askForMfeGeneration(meta) {
   if (!meta && this.existingProject) {
     return;
   }
 
+  const { generateMicroFrontends } = this;
+
   if (generateMicroFrontends === 'ask') {
-    const done = this.async();
-    const prompts = [
-      {
-        type: 'confirm',
-        name: 'generateMfeForEntity',
-        message: 'Do you want to generate micro frontends?',
-        default: true,
-      },
-    ];
-    this.prompt(prompts).then(prompt => {
-      this.configOptions.generateMfeForEntity = prompt.generateMfeForEntity;
-      done();
+    const answers = await this.prompt({
+      type: 'confirm',
+      name: 'generateMfeForEntity',
+      message: 'Do you want to generate micro frontends?',
+      default: true,
     });
+
+    this.configOptions.generateMfeForEntity = answers.generateMfeForEntity;
   } else {
     if (generateMicroFrontends === 'never') {
       this.configOptions.generateMfeForEntity = false;
