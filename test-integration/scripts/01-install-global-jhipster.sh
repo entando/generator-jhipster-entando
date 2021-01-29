5ac9ac5f1ba249038ad213f065cda6281188c2f6
Anthony Viard <anth.viard@gmail.com>
2021-01-29 16:58:42 +0100

    Init default workflow

---
+++ test-integration/scripts/01-install-global-jhipster.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+#-------------------------------------------------------------------------------
+# Install JHipster Generator
+#-------------------------------------------------------------------------------
+echo "*** generator-jhipster: use last version"
+npm install -g generator-jhipster@v"$JHI_VERSION"
+
+#-------------------------------------------------------------------------------
+# Install yeoman
+#-------------------------------------------------------------------------------
+echo "*** yeoman: use last version"
+npm install -g yo
+
+#-------------------------------------------------------------------------------
+# Install JHipster Entando
+#-------------------------------------------------------------------------------
+echo "*** generator-jhipster-entando: use current branch version"
+npm ci
+npm link
+
