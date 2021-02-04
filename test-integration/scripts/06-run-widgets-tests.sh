232bf6a831eb7e4bb438193db86b9f43b890e252
Anthony Viard <anth.viard@gmail.com>
2021-02-04 21:56:41 +0100

    Run tests for all MFE

--- test-integration/scripts/06-run-widgets-tests.sh
+++ test-integration/scripts/06-run-widgets-tests.sh
@@ -7,13 +7,19 @@ else
     echo "*** 00-init-env.sh not found, relying on JHI_* en vars"
 fi
 
-DEFAULT_MFE_NAME=bank-account
+runMFETests() {
+  local entityFolderName="$1"
+
+  for widget in detailsWidget formWidget tableWidget
+  do
+    cd "$JHI_FOLDER_APP/ui/widgets/$entityFolderName/$widget/"
+    npm i && npm test
+  done
+}
+
 #-------------------------------------------------------------------------------
 # Run server test
 #-------------------------------------------------------------------------------
-
-for widget in detailsWidget formWidget tableWidget
-do
-  cd "$JHI_FOLDER_APP/ui/widgets/$DEFAULT_MFE_NAME/$widget/"
-  npm i && npm test
-done
+runMFETests bank-account
+runMFETests label
+runMFETests operation
