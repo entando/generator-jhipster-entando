4bcc443967689936f0ba138b7652aebd7e37f17f
Anthony Viard <anth.viard@gmail.com>
2021-02-23 12:04:24 +0100

    Start backend app when launching e2e tests and run cypress with concurrently

--- test-integration/scripts/07-run-widgets-e2e.sh
+++ test-integration/scripts/07-run-widgets-e2e.sh
@@ -13,11 +13,65 @@ runMFE_E2eTests() {
   for widget in detailsWidget formWidget tableWidget
   do
     cd "$JHI_FOLDER_APP/ui/widgets/$entityFolderName/$widget/"
-    npm start && npm run cypress:run:headless
+    npm run ci:cypress:run
   done
 }
+
+launchCurlTests() {
+    endpointsToTest=("$@")
+    retryCount=1
+    maxRetry=10
+    httpUrl="http://localhost:8081/services/test"
+
+    for endpoint in "${endpointsToTest[@]}"; do
+        curl -fv "$httpUrl$endpoint"
+        status=$?
+        while [ "$status" -ne 0 ] && [ "$retryCount" -le "$maxRetry" ]; do
+            echo "*** [$(date)] Application not reachable yet. Sleep and retry - retryCount =" $retryCount "/" $maxRetry
+            retryCount=$((retryCount+1))
+            sleep 10
+            curl -fv "$httpUrl$endpoint"
+            status=$?
+        done
+
+        if [ "$status" -ne 0 ]; then
+            echo "*** [$(date)] Not connected after" $retryCount " retries."
+            return 1
+        fi
+    done
+    return $?
+}
+
+#-------------------------------------------------------------------------------
+# Run the application
+#-------------------------------------------------------------------------------
+cd "$JHI_FOLDER_APP"
+java \
+    -jar app.jar \
+    --spring.profiles.active="dev" \
+    --logging.level.ROOT=OFF \
+    --logging.level.org.zalando=OFF \
+    --logging.level.org.springframework.web=ERROR \
+    --logging.level.tech.jhipster=OFF \
+    --logging.level.tech.jhipster.sample=OFF &
+echo $! > .pidRunApp
+sleep 40
+
+# Curl some test endpoints
+endpointsToTest=(
+    '/'
+    '/management/health'
+    '/management/health/liveness'
+    '/management/health/readiness'
+)
+launchCurlTests "${endpointsToTest[@]}"
 
 #-------------------------------------------------------------------------------
 # Run e2e tests
 #-------------------------------------------------------------------------------
 runMFE_E2eTests bank-account
+
+resultRunApp=$?
+kill $(cat .pidRunApp)
+
+exit $((resultRunApp))
