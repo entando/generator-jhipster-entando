89c45334528f20652312027766cdb8838b98767f
Anthony Viard <anth.viard@gmail.com>
2021-03-26 10:59:05 +0100

    Improve the CI by reading the JHipster version from the package.json instead of by a hardcoded value

--- test-integration/scripts/01-install-global-jhipster.sh
+++ test-integration/scripts/01-install-global-jhipster.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 set -e
+if [[ -a $(dirname $0)/00-init-env.sh ]]; then
+    source $(dirname $0)/00-init-env.sh
+else
+    echo "*** 00-init-env.sh not found, relying on JHI_* en vars"
+fi
 
 #-------------------------------------------------------------------------------
 # Install JHipster Generator
