50824f08ce19862b23af674b8ff46cfa2565dfc8
Anthony Viard <anth.viard@gmail.com>
2023-03-23 17:24:10 +0100

    [ENG-4717] Update APP prompt file to use JHipster constants instead of hard coded value for options

--- generators/app/prompts.js
+++ generators/app/prompts.js
@@ -1,3 +1,5 @@
+const { MICROSERVICE } = require('generator-jhipster/jdl/jhipster/application-types');
+
 async function askForApplicationType() {
   if (this.existingProject) {
     return;
@@ -5,7 +7,7 @@ async function askForApplicationType() {
 
   // defaulting to microservice for "Which type of application would you like to create?"
   // eslint-disable-next-line no-multi-assign
-  this.applicationType = this.jhipsterConfig.applicationType = 'microservice';
+  this.applicationType = this.jhipsterConfig.applicationType = MICROSERVICE;
 }
 
 module.exports = { askForApplicationType };
