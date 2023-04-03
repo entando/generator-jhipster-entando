a12ecaec9afa6142c7ab167b3c53aa6bb70d79a8
Anthony Viard <anth.viard@gmail.com>
2023-04-03 17:21:33 +0200

    [ENG-4717] Update server prompt to use the JHipster's constants

--- generators/server/prompts.js
+++ generators/server/prompts.js
@@ -2,8 +2,26 @@ const chalk = require('chalk');
 
 const constants = require('generator-jhipster/generators/generator-constants');
 const { serverDefaultConfig } = require('generator-jhipster/generators/generator-defaults');
-const entandoConstants = require('../generator-constants');
+const {
+  CAFFEINE,
+  EHCACHE,
+  HAZELCAST,
+  INFINISPAN,
+  MEMCACHED,
+  REDIS,
+} = require('generator-jhipster/jdl/jhipster/cache-types');
+const { H2_DISK, H2_MEMORY, SQL } = require('generator-jhipster/jdl/jhipster/database-types');
+const { GATEWAY, MICROSERVICE } = require('generator-jhipster/jdl/jhipster/application-types');
+const { OptionNames } = require('generator-jhipster/jdl/jhipster/application-options');
+const databaseTypes = require('generator-jhipster/jdl/jhipster/database-types');
+const cacheProviderTypes = require('generator-jhipster/jdl/jhipster/cache-types');
 const { DEFAULT_SERVER_PROMPTS } = require('../generator-constants');
+const entandoConstants = require('../generator-constants');
+
+const { CACHE_PROVIDER, DATABASE_TYPE, PACKAGE_NAME, DEV_DATABASE_TYPE, PROD_DATABASE_TYPE, SERVER_PORT } =
+  OptionNames;
+const NO_DATABASE = databaseTypes.NO;
+const NO_CACHE_PROVIDER = cacheProviderTypes.NO;
 
 module.exports = {
   /* eslint-disable no-use-before-define */
@@ -16,12 +34,12 @@ function askForServerSideOpts() {
   if (this.existingProject) return undefined;
 
   const { applicationType } = this.jhipsterConfig;
-  const defaultPort = applicationType === 'gateway' ? '8080' : '8081';
+  const defaultPort = applicationType === GATEWAY ? '8080' : '8081';
   const prompts = [
     {
-      when: () => applicationType === 'gateway' || applicationType === 'microservice',
+      when: () => applicationType === GATEWAY || applicationType === MICROSERVICE,
       type: 'input',
-      name: 'serverPort',
+      name: SERVER_PORT,
       validate: input => (/^([0-9]*)$/.test(input) ? true : 'This is not a valid port number.'),
       message:
         'As you are running in a microservice architecture, on which port would like your server to run? It should be unique to avoid port conflicts.',
@@ -29,7 +47,7 @@ function askForServerSideOpts() {
     },
     {
       type: 'input',
-      name: 'packageName',
+      name: PACKAGE_NAME,
       validate: input =>
         /^([a-z_]{1}[a-z0-9_]*(\.[a-z_]{1}[a-z0-9_]*)*)$/.test(input)
           ? true
@@ -40,23 +58,23 @@ function askForServerSideOpts() {
     },
     {
       type: 'list',
-      name: 'databaseType',
+      name: DATABASE_TYPE,
       message: `Which ${chalk.yellow('*type*')} of database would you like to use?`,
       choices: answers => {
         const opts = [];
         if (!answers.reactive) {
           opts.push({
-            value: 'sql',
-            name: 'SQL (H2, PostgreSQL, MySQL)',
+            value: SQL,
+            name: 'SQL (H2, PostgreSQL, MySQL, MariaDB, Oracle, MSSQL)',
           });
         } else {
           opts.push({
-            value: 'sql',
-            name: 'SQL (H2, PostgreSQL, MySQL)',
+            value: SQL,
+            name: 'SQL (H2, PostgreSQL, MySQL, MariaDB, MSSQL)',
           });
         }
         opts.push({
-          value: 'no',
+          value: NO_DATABASE,
           name: 'No database',
         });
         return opts;
@@ -64,26 +82,26 @@ function askForServerSideOpts() {
       default: serverDefaultConfig.databaseType,
     },
     {
-      when: response => response.databaseType === 'sql',
+      when: response => response.databaseType === SQL,
       type: 'list',
-      name: 'prodDatabaseType',
+      name: PROD_DATABASE_TYPE,
       message: `Which ${chalk.yellow('*production*')} database would you like to use?`,
       choices: answers => (answers.reactive ? constants.R2DBC_DB_OPTIONS : entandoConstants.SQL_DB_OPTIONS),
       default: serverDefaultConfig.prodDatabaseType,
     },
     {
-      when: response => response.databaseType === 'sql',
+      when: response => response.databaseType === SQL,
       type: 'list',
-      name: 'devDatabaseType',
+      name: DEV_DATABASE_TYPE,
       message: `Which ${chalk.yellow('*development*')} database would you like to use?`,
       choices: response =>
         [
           {
-            value: 'h2Disk',
+            value: H2_DISK,
             name: 'H2 with disk-based persistence',
           },
           {
-            value: 'h2Memory',
+            value: H2_MEMORY,
             name: 'H2 with in-memory persistence',
           },
         ].concat(constants.SQL_DB_OPTIONS.find(it => it.value === response.prodDatabaseType)),
@@ -92,45 +110,45 @@ function askForServerSideOpts() {
     {
       when: answers => !answers.reactive,
       type: 'list',
-      name: 'cacheProvider',
+      name: CACHE_PROVIDER,
       message: 'Which cache do you want to use? (Spring cache abstraction)',
       choices: [
         {
-          value: 'ehcache',
+          value: EHCACHE,
           name: 'Ehcache (local cache, for a single node)',
         },
         {
-          value: 'caffeine',
+          value: CAFFEINE,
           name: 'Caffeine (local cache, for a single node)',
         },
         {
-          value: 'hazelcast',
+          value: HAZELCAST,
           name: 'Hazelcast (distributed cache, for multiple nodes, supports rate-limiting for gateway applications)',
         },
         {
-          value: 'infinispan',
+          value: INFINISPAN,
           name: 'Infinispan (hybrid cache, for multiple nodes)',
         },
         {
-          value: 'memcached',
+          value: MEMCACHED,
           name: 'Memcached (distributed cache) - Warning, when using an SQL database, this will disable the Hibernate 2nd level cache!',
         },
         {
-          value: 'redis',
+          value: REDIS,
           name: 'Redis (distributed cache)',
         },
         {
-          value: 'no',
+          value: NO_CACHE_PROVIDER,
           name: 'No cache - Warning, when using an SQL database, this will disable the Hibernate 2nd level cache!',
         },
       ],
-      default: applicationType === 'microservice' ? 1 : serverDefaultConfig.cacheProvider,
+      default: applicationType === MICROSERVICE ? 1 : serverDefaultConfig.cacheProvider,
     },
     {
       when: answers =>
-        ((answers.cacheProvider !== 'no' && answers.cacheProvider !== 'memcached') ||
-          applicationType === 'gateway') &&
-        answers.databaseType === 'sql' &&
+        ((answers.cacheProvider !== NO_CACHE_PROVIDER && answers.cacheProvider !== MEMCACHED) ||
+          applicationType === GATEWAY) &&
+        answers.databaseType === SQL &&
         !answers.reactive,
       type: 'confirm',
       name: 'enableHibernateCache',
@@ -152,13 +170,17 @@ function askForServerSideOpts() {
 
     this.packageName = this.jhipsterConfig.packageName = answers.packageName;
     this.serverPort = this.jhipsterConfig.serverPort = answers.serverPort || '8080';
-    this.cacheProvider = this.jhipsterConfig.cacheProvider = !answers.reactive ? answers.cacheProvider : 'no';
+    this.cacheProvider = this.jhipsterConfig.cacheProvider = !answers.reactive
+      ? answers.cacheProvider
+      : NO_CACHE_PROVIDER;
     this.enableHibernateCache = this.jhipsterConfig.enableHibernateCache = !!answers.enableHibernateCache;
-    this.databaseType = this.jhipsterConfig.databaseType = answers.databaseType;
-    this.devDatabaseType = this.jhipsterConfig.devDatabaseType = answers.devDatabaseType;
-    this.prodDatabaseType = this.jhipsterConfig.prodDatabaseType = answers.prodDatabaseType;
+
+    const { databaseType } = answers;
+    this.databaseType = this.jhipsterConfig.databaseType = databaseType;
+    this.devDatabaseType = this.jhipsterConfig.devDatabaseType = answers.devDatabaseType || databaseType;
+    this.prodDatabaseType = this.jhipsterConfig.prodDatabaseType = answers.prodDatabaseType || databaseType;
     this.searchEngine = this.jhipsterConfig.searchEngine = answers.searchEngine;
-    this.buildTool = this.jhipsterConfig.buildTool = 'maven';
+    this.buildTool = this.jhipsterConfig.buildTool = answers.buildTool;
   });
 }
 
